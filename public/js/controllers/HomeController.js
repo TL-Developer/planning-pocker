79f0d8757ae74d48d22ac665b04a598a7de088d2
tiago.lima <tiago.lima>
2016-10-13 18:02:31 -0300

    inserindo componentes e css

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -8,17 +8,17 @@ angular.module('planning-pocker').controller('HomeController', ['$scope','$http'
     $scope.usuario = JSON.parse(localStorage.getItem('usuario_pocker'));
 
     $scope.send_card = function(card) {
-      var cards_checked = document.querySelectorAll('.ng-not-empty');
+      var cards_checked = document.querySelectorAll('.md-checked');
 
       if(cards_checked.length > 1){
         var total = 0;
         for(var i = 0; i < cards_checked.length; i++){
-          var card_number = parseInt(cards_checked[i].defaultValue);
+          var card_number = parseInt(cards_checked[i].innerText);
           total +=card_number;
         }
         socket.emit('send:card', total, $scope.usuario.name);
       }else {
-        var card_number = parseInt(cards_checked[0].defaultValue);
+        var card_number = parseInt(cards_checked[0].innerText);
         socket.emit('send:card', card_number, $scope.usuario.name);
       }
       clean_checks();
@@ -45,8 +45,9 @@ angular.module('planning-pocker').controller('HomeController', ['$scope','$http'
 
   // Limpa checkeds
   function clean_checks() {
-    for(var i = 0; i < document.getElementsByTagName('input').length; i++){
-      document.getElementsByTagName('input')[i].checked = false;
+    var cards_checked = document.querySelectorAll('.md-checked');
+    for(var i = 0; i < cards_checked.length; i++){
+      cards_checked[i].classList.remove('md-checked');
     }
   };
 
